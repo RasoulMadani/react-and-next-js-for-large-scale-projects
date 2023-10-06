668586016df6b1a703d6f2b39f4c26b05fe8c5cd
Rasoulmadani <ali.rahbary@chmail.ir>
2023-10-06 16:59:10 +0330

    اضافه کردن گوگل فونت به طرح

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import './globals.css'
-
-
+import {Figtree} from 'next/font/google';
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  weight: ['300','400','500','600','700','800','900'],
+  variable: "--font-figtree"
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="fa" dir='rtl'>
+    <html lang="fa" dir='rtl' className={`${figtree.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className='bg-gray-200 flex items-center justify-center  h-20'>
           header
