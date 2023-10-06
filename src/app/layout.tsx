b295e9a38da4235652dae06478e6671d9af5772e
Rasoulmadani <ali.rahbary@chmail.ir>
2023-10-06 17:52:49 +0330

    اضافه کردن فونت محلی

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,46 @@
 import './globals.css'
 import {Figtree} from 'next/font/google';
+import localFont from 'next/font/local';
 const figtree = Figtree({
   display: "swap",
   subsets: ["latin"],
   weight: ['300','400','500','600','700','800','900'],
   variable: "--font-figtree"
+})
+const vazirmatn = localFont({
+  src:[
+    {
+      path:'../../public/fonts/vazirmatn/Vazirmatn-Thin.woff2',
+      weight: '100',
+      style: 'normal'
+    },
+    {
+      path:'../../public/fonts/vazirmatn/Vazirmatn-Light.woff2',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path:'../../public/fonts/vazirmatn/Vazirmatn-Regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path:'../../public/fonts/vazirmatn/Vazirmatn-SemiBold.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path:'../../public/fonts/vazirmatn/Vazirmatn-Bold.woff2',
+      weight: '700',
+      style: 'normal'
+    },
+    {
+      path:'../../public/fonts/vazirmatn/Vazirmatn-Black.woff2',
+      weight: '900',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-vazirmatn'
 })
 export default function RootLayout({
   children,
@@ -12,10 +48,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="fa" dir='rtl' className={`${figtree.variable}`}>
+    <html dir='rtl' className={`${figtree.variable} ${vazirmatn.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className='bg-gray-200 flex items-center justify-center  h-20'>
-          header
+          الله
         </header>
         <div className="flex-1 flex">
           {children}
