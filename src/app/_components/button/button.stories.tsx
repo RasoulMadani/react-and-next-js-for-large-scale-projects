2a2f0afdfbeced11db01c0362b3cad4573786cae
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-02 13:51:27 +0330

    اضافه کردن تیلویند به استوری بوک و حل مشکل نمایش ندادن استایل رنگ ها در استوری بوک

--- src/app/_components/button/button.stories.tsx
+++ src/app/_components/button/button.stories.tsx
@@ -4,6 +4,12 @@ import { Button } from "./button";
 const meta: Meta<typeof Button> = {
   component: Button,
   tags: ["autodocs"],
+  decorators:[
+    (Story)=>{
+        document.documentElement.classList.add('dark');
+        return <Story/>;
+    }
+  ]
 };
 
 export default meta;
