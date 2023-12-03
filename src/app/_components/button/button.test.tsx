8514bd49aacb1b4962ee7644901108a926d7e8ad
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-03 12:31:17 +0330

    تست اضافه کردن دکمه به صفحه و بررسی دیزیبل شدن با ویزگی دیزیبلد

--- src/app/_components/button/button.test.tsx
+++ src/app/_components/button/button.test.tsx
@@ -1,7 +1,16 @@
 import { describe } from "node:test";
-
-describe("Button",()=>{
-    it('testing button',()=>{
-
-    })
-})
+import { Button } from ".";
+import { render, screen } from "@testing-library/react";
+describe("Button Component", () => {
+  test("renders a default button", () => {
+    // const div = document.createElement("div");
+    // ReactDOM.render(<Button>Click here</Button>, div);
+    // document.body.appendChild(div);
+    const { getByText } = render(<Button>Click here</Button>);
+    expect(getByText("Click here")).toBeInTheDocument();
+  });
+  test("disables the button when isDisabled prop is true", () => {
+    render(<Button isDisabled={true}>Click here</Button>);
+    expect(screen.getByRole("button")).toBeDisabled();
+  });
+});
