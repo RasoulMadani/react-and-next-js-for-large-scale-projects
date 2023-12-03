e21136aa10c7f9bdd83e20b13ff4dc8ae4d5c2bb
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-03 14:57:41 +0330

    screen.debug() این متد باعث چاپ کامپوننت در کانسول می شود

--- src/app/_components/button/button.test.tsx
+++ src/app/_components/button/button.test.tsx
@@ -1,23 +1,44 @@
 import { describe } from "node:test";
-import { Button } from ".";
+import { Button } from "./button";
 import { render, screen } from "@testing-library/react";
+import { Variant } from "../types/variant.type";
+
 describe("Button Component", () => {
   test("renders a default button", () => {
-    // const div = document.createElement("div");
-    // ReactDOM.render(<Button>Click here</Button>, div);
-    // document.body.appendChild(div);
-    const { getByText } = render(<Button>Click here</Button>);
-    expect(getByText("Click here")).toBeInTheDocument();
+    const { getByText } = render(<Button>click here</Button>);
+    expect(getByText("click here")).toBeInTheDocument();
   });
+
   test("disables the button when isDisabled prop is true", () => {
-    render(<Button isDisabled={true}>Click here</Button>);
-    expect(screen.getByRole("button")).toBeDisabled();
+    render(<Button isDisabled>click here</Button>);
+    expect(screen.getByText("click here")).toBeDisabled();
   });
-  test("applies the correct css class form different button variants", () => {
-    const { rerender } = render(<Button variant="primary">Click here</Button>);
-    expect(screen.getByRole("button")).toHaveClass("btn-primary");
-   
-    rerender(<Button variant="info">Click here</Button>);
-    expect(screen.getByRole("button")).toHaveClass("btn-info");
+
+  test("applies the correct css class for different button variants", () => {
+    const { rerender } = render(<Button variant="primary">click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass(`btn-primary`);
+
+    rerender(<Button variant="info">click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass(`btn-info`);
+  });
+
+  test("applies the correct css class for different button sizes", () => {
+    const { rerender } = render(<Button size="small">click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass(`btn-sm`);
+
+    rerender(<Button size="large">click here</Button>);
+    expect(screen.getByRole("button")).toHaveClass(`btn-lg`);
+  });
+  test("show rendered button", () => {
+    render(
+      <Button
+        variant="primary"
+        isOutline={true} 
+        size="large" 
+        isDisabled={true}>
+        Click here
+      </Button>
+    );
+    screen.debug();
   });
 });
