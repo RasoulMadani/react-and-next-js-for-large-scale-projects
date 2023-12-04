80c4e0293a9f75bbf674cbaf38438b5b097f8178
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-04 12:53:23 +0330

    اضافه کردن کامپوننت بدج و تست های آن به استوری بوک

--- src/app/(courses)/_components/course-card.tsx
+++ src/app/(courses)/_components/course-card.tsx
@@ -1,3 +1,4 @@
+import { Badge } from "@/app/_components/badge";
 import { CourseSummary } from "@/types/course-summary.interface";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,29 +25,22 @@ const CourseCard: React.FC<CourseCardProps> = ({
         />
       </figure>
       <div className="mt-2 flex gap-2 font-semibold dark:text-info px-3 py-2">
-        {
-            recordStatus
-        }
-        {
-            level
-        }
+        <Badge variant="info">{recordStatus}</Badge>
+        <Badge variant="accent">{level}</Badge>
       </div>
       <div className="card-body">
-        <Link href={`/course/${slug}`}>
-            {title}
-        </Link>
-        <p>
-            {subTitle}
-        </p>
+        <Link href={`/course/${slug}`}>{title}</Link>
+        <p>{subTitle}</p>
         <div>
-            {duration}
-            {basePrice}
+            <Badge variant="warning">
+
+          {duration}
+            </Badge>
+          {basePrice}
         </div>
       </div>
-      <Link href={`/course/${slug}`}
-        className="card-footer justify-center"
-      > 
-            مشاهده ی جزییات دوره
+      <Link href={`/course/${slug}`} className="card-footer justify-center">
+        مشاهده ی جزییات دوره
       </Link>
     </div>
   );
