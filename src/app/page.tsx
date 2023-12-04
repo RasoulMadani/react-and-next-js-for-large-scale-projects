589db688e48d0e2ca2d5d83098b0afdab0fb8c53
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-04 10:33:52 +0330

    استاتیک رندرینگ و ریولدیت کردن صفحه در فواصل زمانی مشخص

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
-import Image from "next/image";
-import { Colors } from "./_components/colors/colors";
-import { Button } from "./_components/button";
+import { CourseSummary } from "@/types/course-summary.interface";
 import HomeHeroSection from "./_components/home-hero-section/home-hero-section";
 
-export default function Home() {
+async function getNewestCourses(count: number): Promise<CourseSummary[]> {
+  const res = await fetch(
+    `https://api.classbon.com/api/courses/newest/${count}`,
+    {
+      // با این مقدار می گوییم که نکست جی اس بعد از ۲۰ ثانیه دوباره درخواست بزند
+      // و صفحه را بسازد و با به روزرسانی صفحه تغییرات را خواهیم دید 
+      // قبل از ۲۰ ثانیه اگر صفحه را به روزرسانی هم کنیم باز تغییرات را نمی بینیم
+      // چون این بیست ثانیه سمت سرور شمارش می شود
+      next:{ 
+        // بر اساس ثانیه است  
+        revalidate: 20 * 60 * 60
+      }
+    }
+  );
+  return res.json();
+}
+export default async function Home() {
+  const newestCourses = await getNewestCourses(4);
+  console.log(newestCourses);
   return (
     <>
-      <HomeHeroSection/>
+      <HomeHeroSection />
+      {
+        newestCourses.map(p=>(
+          <p key={p.title}>{p.title}</p>
+        ))
+      }
     </>
   );
 }
