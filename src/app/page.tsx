ba86d1a9f9744a565bb8ce4d1641280ee0c2b543
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-05 11:58:18 +0330

    پیاده سازی کامپوننت فیچر با دیتای استاتیک

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,21 @@ import { CourseSummary } from "@/types/course-summary.interface";
 import HomeHeroSection from "./_components/home-hero-section/home-hero-section";
 import { CourseCardList } from "./(courses)/_components/course-card-list";
 
-import {IconClock} from './_components/icons/icons'; 
+import { IconClock } from "./_components/icons/icons";
+import { homeFeatures } from "@/data/home-features";
+import Feature from "./_components/feature/feature";
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
     `https://api.classbon.com/api/courses/newest/${count}`,
     {
       // با این مقدار می گوییم که نکست جی اس بعد از ۲۰ ثانیه دوباره درخواست بزند
-      // و صفحه را بسازد و با به روزرسانی صفحه تغییرات را خواهیم دید 
+      // و صفحه را بسازد و با به روزرسانی صفحه تغییرات را خواهیم دید
       // قبل از ۲۰ ثانیه اگر صفحه را به روزرسانی هم کنیم باز تغییرات را نمی بینیم
       // چون این بیست ثانیه سمت سرور شمارش می شود
-      next:{ 
-        // بر اساس ثانیه است  
-        revalidate: 20 * 60 * 60
-      }
+      next: {
+        // بر اساس ثانیه است
+        revalidate: 20 * 60 * 60,
+      },
     }
   );
   return res.json();
@@ -24,18 +26,21 @@ export default async function Home() {
   return (
     <>
       <HomeHeroSection />
+      <section className="dark:bg-base-75 mt-10">
+                <div className="container py-10 flex flex-col lg:flex-row gap-10 xl:gap-5">
+                    {homeFeatures.map((feature) => (
+                        <Feature key={`feature-${feature.title}`} feature={feature} />
+                    ))}
+                </div>
+            </section>
       <section className="container pt-20">
         <div className="text-center xl:text-right">
-          <h2 className="text-2xl font-extrabold">
-            تازه ترین  دوره های آموزشی
-          </h2>
-          <p>
-            برای به روز موندن یاد گرفتن نکته های تازه ضروریه
-          </p>
+          <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
+          <p>برای به روز موندن یاد گرفتن نکته های تازه ضروریه</p>
         </div>
-        <CourseCardList courses={newestCourses}/>
-        <IconClock width={250} height={250} stroke="red"/>
+        <CourseCardList courses={newestCourses} />
+        <IconClock width={250} height={250} stroke="red" />
       </section>
-    </> 
+    </>
   );
 }
