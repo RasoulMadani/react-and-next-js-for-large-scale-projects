5f27842893b62b1b4b3837da400dd6357339147a
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-06 08:17:40 +0330

    نحوه ی استفاده از فایل های .env جلسه ۲۸

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,10 @@ import Feature from "./_components/feature/feature";
 import { Button } from "./_components/button";
 import { BlogPostSummary } from "@/types/blog-post-summary.interface";
 import { BlogPostCardList } from "./(blog)/_components/blog-post-card-list";
+import { API_URL } from "@/configs/global";
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
-    `https://api.classbon.com/api/courses/newest/${count}`,
+    `${API_URL}/courses/newest/${count}`,
     {
       // با این مقدار می گوییم که نکست جی اس بعد از ۲۰ ثانیه دوباره درخواست بزند
       // و صفحه را بسازد و با به روزرسانی صفحه تغییرات را خواهیم دید
@@ -26,7 +27,7 @@ async function getNewestCourses(count: number): Promise<CourseSummary[]> {
 }
 
 async function getNewestPosts(count: number): Promise<BlogPostSummary[]> {
-  const res = await fetch(`https://api.classbon.com/api/blog/newest/${count}`, {
+  const res = await fetch(`${API_URL}/blog/newest/${count}`, {
     // با این مقدار می گوییم که نکست جی اس بعد از ۲۰ ثانیه دوباره درخواست بزند
     // و صفحه را بسازد و با به روزرسانی صفحه تغییرات را خواهیم دید
     // قبل از ۲۰ ثانیه اگر صفحه را به روزرسانی هم کنیم باز تغییرات را نمی بینیم
