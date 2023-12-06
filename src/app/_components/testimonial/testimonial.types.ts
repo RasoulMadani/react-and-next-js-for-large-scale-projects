81a3063e44a0747307d9e9ebbc7aad00bde616ec
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-06 10:36:30 +0330

    ایجاد بخش نظرات کاربران

---
+++ src/app/_components/testimonial/testimonial.types.ts
@@ -0,0 +1,8 @@
+import { Testimonial } from "@/types/testimonial.types";
+
+export type TestimonialProps = {
+  testimonial: Testimonial;
+};
+export type TestimonialListProps = {
+  testimonials: Testimonial[];
+};
