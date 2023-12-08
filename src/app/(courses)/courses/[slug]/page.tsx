e2acc1c0886821c24fac86839ee894ec57154bae
Rasoulmadani <ali.rahbary@chmail.ir>
2023-12-08 06:37:32 +0330

    ساخت کامپوننت آکاردیون در صفحه جزییات محصول

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -5,6 +5,8 @@ import { CourseDetails } from "@/types/course-details.interface";
 import { CourseAside } from "./_components/course-aside/course-aside";
 import { Tab } from "@/types/tab.type";
 import { Tabs } from "@/app/_components/tabs";
+import { Accordion } from "@/app/_components/accordion";
+import { Accordion as AccordionType } from "@/types/accordion.type";
 
 export async function generateStaticParams() {
   const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) => {
@@ -26,6 +28,11 @@ export default async function CourseDetails({
 }) {
   const { slug } = params;
   const course = await getCourse(slug);
+  const faqs: AccordionType[] = course.frequentlyAskedQuestions.map((faq) => ({
+    id: faq.id,
+    title: faq.question,
+    content: faq.answer,
+  }));
   const tabs: Tab[] = [
     {
       label: "مشخصات دوره",
@@ -37,7 +44,7 @@ export default async function CourseDetails({
     },
     {
       label: "سوالات متداول",
-      content: "accordion components",
+      content: <Accordion data={faqs} />,
     },
   ];
   return (
